c4ad528a761d997c31cb6277f4faf3be27d9f63d
Henrik Otterheim <>
2020-03-23 16:09:04 +0100

    Updates for integration with contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -49,17 +49,3 @@ exports.createPages = ({ graphql, actions }) => {
     return null
   })
 }
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
